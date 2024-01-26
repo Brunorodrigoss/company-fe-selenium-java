197ef6d98e5f5fbf7776aba8aebbb42c3cd0adbf
Bruno Rodrigo <bnorodrigo@gmail.com>
2024-01-26 17:12:49 +0000

    Add some classes to be able to test Spontaneous Dialogue

--- src/test/java/Constants/UrlConstants.java
+++ src/test/java/Constants/UrlConstants.java
@@ -1,6 +1,6 @@
 package Constants;
 
 public class UrlConstants {
-    public String baseUrl = "https://www.defined.ai/";
+    public String baseUrl = "";
     public String spontaneousDialogue = baseUrl + "category/spontaneous-dialogue";
 }
