49f679e739eb1868839fa1c5667b82952016f323
Bruno Rodrigo <bnorodrigo@gmail.com>
2024-02-06 15:51:54 +0000

    Refactor submenu

--- src/test/java/Constants/UrlConstants.java
+++ src/test/java/Constants/UrlConstants.java
@@ -1,40 +1,6 @@
 package Constants;
 
 public class UrlConstants {
-    public String baseUrl = "";
-
-    // Speech
-    public String spontaneousDialogue = baseUrl + "category/spontaneous-dialogue";
-    public String scriptedMonologue = baseUrl + "category/scripted-monologue";
-    public String spontaneousIvr = baseUrl + "category/spontaneous-ivr";
-    public String phoneticLexica = baseUrl + "dataset/phonetic-lexica";
-    public String exploreSpeechDatasets = baseUrl + "data-explorer";
-
-    // Live Data
-    public String podcastsDatasets = baseUrl + "dataset/podcast-data";
-    public String liveVoiceAssistantInquiries = baseUrl + "dataset/live-data-voice-assistant";
-
-    // Generative AI
-    public String generativeAiDatasets = baseUrl + "category/generative-ai-datasets";
-    public String healthcarePromptAndResponseData = baseUrl + "dataset/healthcare-prompt-and-response-data";
-    public String stemQuestionAnswerPairs = baseUrl + "dataset/stem-question-answer-pairs-dataset-for-large-language-models";
-    public String generalKnowledgePromptAndResponseDataset = baseUrl + "dataset/general-knowledge-prompt-and-response";
-
-    // Miscellaneous
-    public String healthScreeningReports = baseUrl + "dataset/health-screening-reports";
-    public String medicalUserEngagementAnalytics = baseUrl + "dataset/medical-user-engagement-analytics";
-
-    // Image and Video
-    public String selfiesAndPairedIdPhotos = baseUrl + "dataset/selfies-and-paired-id-photos";
-    public String humanImagesAndVideos = baseUrl + "dataset/human-images-and-videos";
-    public String retailImages = baseUrl + "dataset/retail-images";
-    public String sensitiveImagesAndVideos = baseUrl + "dataset/sensitive-images-and-videos";
-    public String shortFormVideos = baseUrl + "dataset/short-form-videos";
-    public String pointOfViewImagesDataset = baseUrl + "dataset/point-of-view-images";
-    public String producedVideoContent = baseUrl + "dataset/produced-content";
-
-    // Text
-    public String aspectBasedSentimentAnalysis = baseUrl + "dataset/aspect-based-sentiment-analysis";
-    public String namedEntityRecognition = baseUrl + "dataset/named-entity-recognition-dataset";
-    public String parallelCorpora = baseUrl + "dataset/translation-dataset-multilingual-parallel-corpora";
+    public String baseUrl = "https://www.defined.ai/";
+    public DatasetsConstants datasets = new DatasetsConstants();
 }
