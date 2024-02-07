007efc2647de19d8c27adac0477815ab13c2c4b3
Bruno Rodrigo <bnorodrigo@gmail.com>
2024-02-07 16:12:23 +0000

    Change setup function

--- src/test/java/HomepageTest.java
+++ src/test/java/HomepageTest.java
@@ -1,45 +1,24 @@
-import Constants.Constants;
+import Base.BaseTestClass;
 import Pages.HomepagePage;
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.concurrent.TimeUnit;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class HomepageTest {
-    private final Constants constants = new Constants();
-    private WebDriver driver;
-    private HomepagePage homepagePage;
+public class HomepageTest extends BaseTestClass {
 
-    @BeforeEach
+    @BeforeAll
     public void setup() {
-        System.setProperty(constants.config.driverKey, constants.config.driverPath);
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(constants.config.driveImplicitlyWait, TimeUnit.SECONDS);
+        setupDriver();
 
         homepagePage = new HomepagePage(driver);
-        homepagePage.setup(constants.url.baseUrl);
+        homepagePage.setup();
     }
 
-    @AfterEach
+    @AfterAll
     public void tearDown() {
         this.driver.quit();
     }
 
-    @Test
-    @DisplayName("When the user select the option Spontaneous Dialogue on menu, " +
-            "Then the application will be redirect to Spontaneous Dialogue page")
-    public void whenUserSelectTheOptionSpontaneousDialogue_ThenTheApplicationWillRedirectToSpontaneousDialoguePage() {
-        homepagePage.menu.datasets.speech.spontaneousDialogue();
-    }
 
-    @Test
-    @DisplayName("When the user select the option Scripted Monologue on menu, " +
-            "Then the application will be redirect to Scripted Monologue page")
-    public void whenUserSelectTheOptionScriptedMonologue_ThenTheApplicationWillRedirectToScriptedMonologuePage() {
-        homepagePage.menu.datasets.speech.scriptedMonologue();
-    }
 
     @Test
     @DisplayName("When the user select the option Spontaneous IVR on menu, " +
