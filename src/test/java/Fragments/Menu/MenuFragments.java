e8037c40c5dc165dbdfd3e77c531eecf2ad75dc1
Bruno Rodrigo <bnorodrigo@gmail.com>
2024-02-07 14:54:07 +0000

    Add Industries menu

--- src/test/java/Fragments/Menu/MenuFragments.java
+++ src/test/java/Fragments/Menu/MenuFragments.java
@@ -6,16 +6,18 @@ import org.openqa.selenium.WebDriver;
 public class MenuFragments {
     WebDriver driver;
     By datasetsMenu = By.id("item-menu-1");
-    By industriesMenu = By.id("");
+    By industriesMenu = By.id("item-menu-20");
     By servicesMenu = By.id("");
     By resourcesMenu = By.id("");
     By companyMenu = By.id("");
     By ethicalAiMenu = By.id("");
 
     public DatasetsMenuFragments datasets;
+    public IndustriesMenuFragments industries;
 
     public MenuFragments(WebDriver driver){
         this.driver = driver;
         this.datasets = new DatasetsMenuFragments(this.driver, datasetsMenu);
+        this.industries = new IndustriesMenuFragments(this.driver, industriesMenu);
     }
 }
