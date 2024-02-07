ed05cf57ff824bd4da199da402094c1ec183460e
Bruno Rodrigo <bnorodrigo@gmail.com>
2024-02-07 16:13:18 +0000

    Extend class to use BasePage

--- src/test/java/Pages/HomepagePage.java
+++ src/test/java/Pages/HomepagePage.java
@@ -1,19 +1,18 @@
 package Pages;
 
+import Base.BasePage;
 import Fragments.Menu.MenuFragments;
 import org.openqa.selenium.WebDriver;
 
 
-public class HomepagePage {
-    WebDriver driver;
-    public MenuFragments menu;
+public class HomepagePage extends BasePage {
 
     public HomepagePage(WebDriver driver) {
         this.driver = driver;
         this.menu = new MenuFragments(this.driver);
     }
 
-    public void setup(String url) {
-        this.driver.get(url);
+    public void setup() {
+        this.driver.get(constants.url.baseUrl);
     }
 }
